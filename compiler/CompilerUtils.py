c0b20bd16eb1452d6884d0fc8df3f3bd0dc65c7f
Ravi Teja <bsravi.tez@gmail.com>
2016-04-04 12:26:42 +0530

    Multiple test cases support added

--- compiler/CompilerUtils.py
+++ compiler/CompilerUtils.py
@@ -3,9 +3,11 @@ import subprocess
 import random
 import os
 
+
 class Language(Enum):
     PYTHON = 1
     JAVA = 2
+
 
 class TestCase:
     input_data = None
@@ -18,8 +20,25 @@ class TestCase:
     def getInput(self):
         return self.input_data
 
-    def getOuput(self):
+    def getOutput(self):
         return self.output_data
+
+
+def generateTestCases(inputString,outputString):
+    test_cases = []
+    inputs = [x.strip() for x in inputString.split(sep='$END')]
+    outputs = [x.strip() for x in outputString.split(sep='$END')]
+    if len(inputs) is not len(outputs):
+        return None  # need a better way :(
+    else:
+        # removing last blank space (if any) generated due to split function
+        if len(inputs[len(inputs)-1]) == 0:
+            del inputs[len(inputs)-1]
+            del outputs[len(outputs)-1]
+        for i in range(len(inputs)):
+            test_cases.append(TestCase(inputs[i],outputs[i]))
+        return test_cases
+
 
 class Compiler:
     code = None
@@ -104,6 +123,32 @@ class Compiler:
         self.hasFile = False
         self.filename = None
 
+    def checkOutputs(self):
+        index = 0
+        values = []
+        for testcase in self.test_cases:
+            expected_output = testcase.getOutput()
+            actual_output = self.outputs[index].strip()
+
+            # Debug ONLY..........................
+            print("EX: "+expected_output)
+            print("len : "+str(len(str(expected_output))))
+            print("AC: "+actual_output)
+            print("len : "+str(len(str(actual_output))))
+            # Debug ONLY............................
+
+            values.append(expected_output is actual_output)
+            index += 1
+
+        # Debug ONLY .....................
+        print("Values .... ")
+        for v in values:
+            print(str(v))
+        # Debug ONLY .....................
+
+        return values
+
+
     def execute(self):
         if self.language is not None:
             if not self.hasFile or self.filename is None:
@@ -114,6 +159,7 @@ class Compiler:
                     self.outputs = []
                 if self.errors is None:
                     self.errors = []
+                print("Testcases # : "+str(len(self.test_cases)))
                 for test_case in self.test_cases:
                     process = subprocess.Popen(command,stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
                     o,e = process.communicate(str(test_case.getInput()).encode('utf-8'))
--- compiler/views.py
+++ compiler/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from . import forms
+from . import CompilerUtils
 from .CompilerUtils import Compiler,TestCase,Language
 
 # Create your views here.
@@ -9,10 +10,13 @@ def testpage(request):
     if request.method == 'POST':
         form = forms.CodeExecutorForm(request.POST)
         if form.is_valid():
+            executor = Compiler()
             code = form.cleaned_data['code']
             input_data = form.cleaned_data['input']
             expected_output = form.cleaned_data['output']
-            testcase = TestCase(input_data,expected_output)
+            test_cases = CompilerUtils.generateTestCases(input_data,expected_output)
+            for testcase in test_cases:
+                executor.addTestCase(testcase)
             lan = Language(int(form.cleaned_data['language']))
             has_template = form.cleaned_data['has_template']
             if has_template:
@@ -22,7 +26,6 @@ def testpage(request):
                 return render(request,'generic_error.html',template_data)
             else:
                 if lan == Language.PYTHON:
-                    executor = Compiler()
                     executor.setCode(code)
                     executor.setLanguage(lan)
                     executor.addTestCase(testcase)
@@ -31,11 +34,21 @@ def testpage(request):
                     executor.execute()
                     executor.deleteCodeFile()
                     if executor.hasExecuted:
-                        if executor.hasErrors:
-                            template_data['error'] = executor.getErrors()[0]
-                        else:
-                            template_data['error'] = "No errors :)"
-                        template_data['output'] = executor.getOutput()[0]
+                        checked_values = executor.checkOutputs()
+
+                        for i in range(len(checked_values)):
+                            if checked_values[i]:
+                                template_data['t'+str(i)] = "success!"
+                            else:
+                                template_data['t'+str(i)] = "failed!"
+
+                        for i in range(len(executor.getOutput())):
+                            if executor.hasErrors:
+                                template_data['error'+str(i)] = executor.getErrors()[i]
+                            else:
+                                template_data['error'+str(i)] = "No errors :)"
+                            template_data['output'+str(i)] = executor.getOutput()[i]
+
                         return render(request,'OutputView.html',template_data)
                     else:
                         return render(request,'generic_error.html',{'error':'Sorry! Execution failed'})
